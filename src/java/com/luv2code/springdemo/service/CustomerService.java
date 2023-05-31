a09a7da21431b1b3300b9a508f3894fdedae8a41
Kalin Ivanov <kalin2578@gmail.com>
2023-06-01 00:13:32 +0300

    Refactor some issues

--- src/java/com/luv2code/springdemo/service/CustomerService.java
+++ src/java/com/luv2code/springdemo/service/CustomerService.java
@@ -1,18 +1,14 @@
 package com.luv2code.springdemo.service;
 
 import com.luv2code.springdemo.entity.Customer;
-import com.luv2code.springdemo.util.SortUtils;
-
 import java.util.List;
 
 /**
- *
  * @author kalin
  */
 public interface CustomerService {
-    public List<Customer> getCustomers();
-    public void saveCustomer(Customer theCustomer);
-    public Customer getCustomer(int theId);
-    public void deleteCustomer(int theId);
-
+    List<Customer> getCustomers();
+    void saveCustomer(Customer theCustomer);
+    Customer getCustomer(int theId);
+    void deleteCustomer(int theId);
 }
